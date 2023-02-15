d8e0a2a3d59dc69e2b24f3795a817829c2985cc8
Mohamed Ebdalla <mohamed.ebdalla@ucalgary.ca>
2023-02-15 16:39:16 -0700

    Completed ex2.2.py and ex2.4.py

---
+++ ex2.4.py
@@ -0,0 +1,63 @@
+import sys
+import json
+import time
+import matplotlib.pyplot as plt
+
+sys.setrecursionlimit(20000)
+
+def modified_func1(arr, low, high):
+    if high - low + 1 <= 20:
+        return insertion_sort(arr, low, high)
+    else:
+        pi = func2(arr, low, high)
+        modified_func1(arr, low, pi-1)
+        modified_func1(arr, pi+1, high)
+        return arr
+
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high -= 1
+        while low <= high and array[low] <= p:
+            low += 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+
+
+def insertion_sort(arr, low, high):
+    for i in range(low+1, high+1):
+        key = arr[i]
+        j = i-1
+        while j >= low and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+    return arr
+
+# open and load the contents of the file
+with open("ex2.2.json", "r") as file:
+    contents = json.load(file)
+
+    arr_of_times = []
+    input_labels = ["input 1", "input 2", "input 3", "input 4", "input 5", "input 6", "input 7", "input 8", "input 9", "input 10", ]
+    for input in contents:
+        size = len(input)
+        start = time.time()
+        modified_func1(input, 0, size - 1)
+        end = time.time()
+        arr_of_times.append(end - start)
+    print(arr_of_times)
+
+    plt.plot(input_labels, arr_of_times)
+    plt.title("Inputs vs Time Taken")
+    plt.xlabel("Inputs")
+    plt.ylabel("Time Taken (s)")
+    plt.show()
